2e6ea96415d747dd99c7a5b15b30d47de6da5a8d
rfhits <wysj0117@gmail.com>
2024-06-06 11:39:48 +0800

    add case for push after receive partially ack

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -19,27 +19,50 @@ void TCPSender::push( const TransmitFunction& transmit )
 {
   // Your code here.
   if ( is_FIN_acked ) {
+    cerr << "FIN is acked, sender shouldn't send data" << endl;
     cerr << "I don't know how to deal with that" << endl;
     return;
   }
-
-  // [last_ackno, last_ackno + wnd_size)
-  // send outstanding segments overlap with the interval
 
   auto remain_wnd_size = wnd_size_ == 0 ? 1 : wnd_size_;
 
   bool has_SYN_sent = false;
 
-  bool is_input_finished = input_.writer().is_closed();
-  uint64_t remain_data_size = ( abs_last_ackno_ == 0 ) + input_.reader().bytes_buffered() + is_input_finished;
-
   uint64_t abs_cur_seqno = abs_last_ackno_; // send from this seqno, change in while loop
+
+  // [last_ackno, last_ackno + wnd_size)
+  // send outstanding segments overlap with the interval
+  if ( abs_last_ackno_ < abs_exp_ackno_ ) {
+    auto it = ost_segs_.begin();
+    while ( it != ost_segs_.end() && remain_wnd_size > 0 ) {
+      if ( it->first + it->second.sequence_length() > abs_last_ackno_ ) {
+        transmit( it->second );
+        has_SYN_sent = true;
+        if ( !timer_.is_running_ ) {
+          timer_.reset( cur_RTO_ms_ );
+        }
+        uint64_t useful_len_in_seg = it->first + it->second.sequence_length() - abs_last_ackno_;
+        remain_wnd_size = ( useful_len_in_seg >= remain_wnd_size ) ? 0 : remain_wnd_size - useful_len_in_seg;
+        abs_cur_seqno = it->first + it->second.sequence_length();
+        it++;
+      } else {
+        cerr << "error: exist seg in ost_seg be acked" << endl;
+        it++;
+      }
+    }
+  }
+  if ( remain_wnd_size == 0 ) {
+    return;
+  }
+
+  bool is_input_finished = input_.writer().is_closed();
+  uint64_t remain_data_size = ( !has_SYN_sent & abs_cur_seqno == 0 ) + input_.reader().bytes_buffered() + ( is_input_finished & !has_FIN_sent_);
 
   while ( remain_data_size > 0 && remain_wnd_size > 0 ) {
     TCPSenderMessage cur_msg;
     cur_msg.seqno = Wrap32::wrap( abs_cur_seqno, isn_ );
 
-    if ( abs_last_ackno_ == 0 && !has_SYN_sent ) {
+    if ( abs_cur_seqno == 0 && !has_SYN_sent ) {
       cur_msg.SYN = true;
       cur_msg.seqno = isn_;
       remain_data_size -= 1;
@@ -64,7 +87,7 @@ void TCPSender::push( const TransmitFunction& transmit )
       remain_wnd_size -= cur_msg.payload.size();
       remain_data_size -= cur_msg.payload.size();
 
-      if ( remain_wnd_size > 0 && input_.reader().is_finished() && is_input_finished ) {
+      if ( remain_wnd_size > 0 && input_.reader().is_finished() ) {
         remain_wnd_size -= 1;
         cur_msg.FIN = true;
         remain_data_size = 0;
@@ -101,7 +124,7 @@ void TCPSender::receive( const TCPReceiverMessage& msg )
 
   uint64_t abs_rcv_ackno = 0;
 
-  if ( msg.ackno == nullopt ) {
+  if ( !msg.ackno.has_value() ) {
     abs_last_ackno_ = 0;
     abs_rcv_ackno = 0;
     ost_segs_.clear();
@@ -113,7 +136,7 @@ void TCPSender::receive( const TCPReceiverMessage& msg )
 
   if ( abs_rcv_ackno <= abs_last_ackno_ ) {
     return;
-  } else {
+  } else { // new segment get acked
     cur_RTO_ms_ = initial_RTO_ms_;
     retx_cnt_ = 0;
     is_con_retx_ = false;
